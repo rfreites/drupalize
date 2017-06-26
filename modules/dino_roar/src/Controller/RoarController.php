087e213895d7884ba655c7f3fb01cd534c84194a
Ronny <ronnyangelo.freites@gmail.com>
2017-06-25 19:26:11 -0600

    Get service from service container

--- modules/dino_roar/src/Controller/RoarController.php
+++ modules/dino_roar/src/Controller/RoarController.php
@@ -8,16 +8,39 @@
 
 namespace Drupal\dino_roar\Controller;
 
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
 use Drupal\dino_roar\Jurassic\RoarGenerator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-class RoarController
+class RoarController extends ControllerBase
 {
+    private $roarGenerator;
+    protected $loggerFactory;
+
+    public function __construct(RoarGenerator $roarGenerator, LoggerChannelFactoryInterface  $loggerFactory)
+    {
+        $this->roarGenerator = $roarGenerator;
+        $this->loggerFactory = $loggerFactory;
+    }
+
     public function roar($count)
     {
-        $roarGenerator = new RoarGenerator();
-        $roar = $roarGenerator->getRoar($count);
+        $roar = $this->roarGenerator->getRoar($count);
+        $this->loggerFactory->get('default')
+            ->debug($roar);
 
         return new Response($roar);
+    }
+
+    //override method to create from the ControllerBase and access to the container services
+    public static function create(ContainerInterface $container)
+    {
+        $roarGenerator = $container->get('dino_roar.roar_generator');
+
+        $loggerFactory = $container->get('logger.factory');
+
+        return new static($roarGenerator, $loggerFactory);
     }
 }
